21aa1b24240772d449dbe36ecd20f8a0504c0a28
Johan Gavelan <johangavelan@gmail.com>
2020-07-03 22:04:07 -0500

    Google fonts added - Header stylized

--- functions.php
+++ functions.php
@@ -11,7 +11,10 @@ function vissentia_scripts() {
     wp_register_script('vissentia-script', get_template_directory_uri() . '/assets/js/main.js', ['jquery']. 1, true);
     wp_enqueue_script('vissentia-script');
 
+    wp_enqueue_style('google_web_fonts', 'https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@500&family=Noto+Sans+HK:wght@400;500;700&family=Nunito+Sans:wght@400;600&display=swap');
+
 }
+
 
 
 add_action('wp_enqueue_scripts', 'vissentia_scripts');
