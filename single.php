2be3703f89bfd892e196b08c9dc8d4b72246ae00
Johan Gavelan <johangavelan@gmail.com>
2020-07-09 21:00:11 -0500

    image added

--- single.php
+++ single.php
@@ -2,6 +2,12 @@
 
     <h1><?php the_title(); ?></h1>
 
+    <?php if(has_post_thumbnail()) : ?>
+
+        <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title();?>">
+
+    <?php endif; ?><br>
+
     <?php get_template_part('includes/section','blogcontent'); ?>
 
     <?php wp_link_pages(); ?>
