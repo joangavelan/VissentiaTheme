333d42ea133a43adee2a3d21805be7a7f8b63876
Johan Gavelan <johangavelan@gmail.com>
2020-07-10 19:16:31 -0500

    thumbnails added to archive and page

--- includes/section-archive.php
+++ includes/section-archive.php
@@ -2,6 +2,12 @@
 
     <h3><?php the_title();?></h3>
 
+    <?php if(has_post_thumbnail()) : ?>
+
+        <img src="<?php the_post_thumbnail_url('blog-small');?>" alt="<?php the_title();?>">
+
+    <?php endif; ?>
+
     <?php the_excerpt(); ?>
 
     <a href="<?php the_permalink();?>">Read More</a>
