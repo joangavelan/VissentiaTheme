c6b48d709c90ce81bbd77cfcd8dc4625ddd36a5f
Johan Gavelan <johangavelan@gmail.com>
2020-07-10 21:24:37 -0500

    templates created

---
+++ templates/blog.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+Template Name: Blog
+*/
+?>
+
+<?php get_header(); ?>
+
+    <?php the_title(); ?>
+
+    <?php get_template_part('includes/section','archive'); ?>
+
+
+<?php get_footer(); ?>
