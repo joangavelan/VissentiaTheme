3eeaa6f168d59746a85790eb36e550b89394b8f5
Johan Gavelan <johangavelan@gmail.com>
2020-07-13 09:09:22 -0500

    footer added

--- footer.php
+++ footer.php
@@ -1,7 +1,26 @@
 <?php wp_footer(); ?>
 
     </main>
-    <div class="mg-md"></div>
+    <div class="mg-bg"></div>
+
+    <footer class="footer">
+        <div class="footer__logo-box">
+            <h2 class="footer__title">Vitae<span>Essentia</span></h2>
+        </div>
+        <!-- <div class="footer__nav">
+            <ul class="footer__menu">
+                <li class="footer__item"><a href="#" class="footer__link">Home</a></li>
+                <li class="footer__item"><a href="#" class="footer__link">Superalimentos</a></li>
+                <li class="footer__item"><a href="#" class="footer__link">Salud</a></li>
+                <li class="footer__item"><a href="#" class="footer__link">Hierbas</a></li>
+                <li class="footer__item"><a href="#" class="footer__link">Frutas</a></li>
+                <li class="footer__item"><a href="#" class="footer__link">Blog</a></li>
+                <li class="footer__item"><a href="#" class="footer__link">Contact</a></li>
+            </ul>
+            </nav>
+        </div> -->
+        <div class="footer__rights"><a href="#" class="footer__privacy">Privacy Policy</a>  |   <span class="footer__copyright">2020 © Vissentia</span></div>
+    </footer>
 
 </script>
 </body>
