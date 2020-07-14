e6b3789e90de473481b2ea023d76750ce84c4aec
Johan Gavelan <johangavelan@gmail.com>
2020-07-13 23:27:11 -0500

    Single post format developed - fully responsive

--- single.php
+++ single.php
@@ -1,15 +1,33 @@
 <?php get_header(); ?>
 
-    <h1><?php the_title(); ?></h1>
+    <div class="mg-bg"></div>
+    <section class="feed">
+        <div class="feed__main">
+            <article class="post">
+                <h1 class="post__title"><?php the_title(); ?></h1>
 
-    <?php if(has_post_thumbnail()) : ?>
+                <a href="#" class="category-meta">Superalimentos</a>
+                
+                <?php if(has_post_thumbnail()) : ?>
+            
+                    <img src="<?php the_post_thumbnail_url('blog-large');?>" class="post__img" alt="<?php the_title();?>">
+            
+                <?php endif; ?><br>
+            
+                <?php get_template_part('includes/section','blogcontent'); ?>
+            
+                <?php wp_link_pages(); ?>
+            </article>
+        </div>
 
-        <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title();?>">
-
-    <?php endif; ?><br>
-
-    <?php get_template_part('includes/section','blogcontent'); ?>
-
-    <?php wp_link_pages(); ?>
+        <div class="feed__sidebar">
+            <div class="mg-sm"></div>
+            <aside class="sidebar">
+                <div class="sidebar__ad"></div>
+                <div class="mg-bg"></div>
+                <div class="sidebar__banner"></div>
+            </aside>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
