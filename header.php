1c227d20c1673e672a311355b4cf2e5cc25c8ce1
Johan Gavelan <johangavelan@gmail.com>
2020-07-15 11:32:54 -0500

    dynamic wordpress menu configured

--- header.php
+++ header.php
@@ -16,56 +16,22 @@
 <header class="header">
 
     <nav class="header__nav">
-    <a href="http://vissentia.test/"><div class="header__logo">Vissentia</div></a>
+    <div class="header__logo"><a href="">Vissentia</a></div>
     <label for="btn" class="icon">
         <span class="fa fa-bars"></span>
     </label>
     <input type="checkbox" id="btn">
-        <ul>
-            <li><a href="http://vissentia.test/">Home</a></li>
-            <li><a href="#">Nosotros</a></li>
-            <li>
-            <label for="btn-1" class="show">Superalimentos</label>
-            <a href="#">Superalimentos</a>
-            <input type="checkbox" id="btn-1">
-                <!-- <ul>
-                    <li><a href="#">Açai</a></li>
-                    <li><a href="#">Camu Camu</a></li>
-                    <li><a href="#">Quinoa</a></li>
-                </ul> -->
-            </li>
-            <li>
-            <label for="btn-2" class="show">Plantas Medicinales</label>
-            <a href="#">Plantas Medicinales</a>
-            <input type="checkbox" id="btn-2">
-                <!-- <ul>
-                    <li><a href="#">Europa</a></li>
-                    <li>
-                    <label for="btn-3" class="show show-sub">América +</label>
-                    <a href="#">América<span class="fa fa-plus"></span></a>
-                    <input type="checkbox" id="btn-3">
-                        <ul>
-                            <li><a href="#">Eucalipto</a></li>
-                            <li><a href="#">Jenjibre</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Asia</a></li>
-                    <li><a href="#">África</a></li>
-                </ul> -->
-            </li>
-            <li>
-            <label for="btn-4" class="show">Extractos</label>
-            <a href="#">Extractos</a>
-            <input type="checkbox" id="btn-4">
-                <!-- <ul>
-                    <li><a href="#">Vision</a></li>
-                    <li><a href="#">Piel</a></li>
-                    <li><a href="#">Colesterol</a></li>
-                </ul> -->
-            </li>
-            <li><a href="http://vissentia.test/blog/">Blog</a></li>
-            <li><a href="http://vissentia.test/contacto/">Contacto</a></li>
-        </ul>
+ 
+    
+    <?php
+        wp_nav_menu (
+    
+            array(
+                'theme_location' => 'top-menu',
+            )
+    
+        );
+        ?>
 
     </nav>
 
