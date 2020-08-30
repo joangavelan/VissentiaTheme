ef127291e368e02c9d5ae13e2537c6e4374ba8c2
Johan Gavelan <johangavelan@gmail.com>
2020-08-30 02:02:28 -0500

    testing hero

--- includes/section-hero.php
+++ includes/section-hero.php
@@ -1,5 +1,5 @@
 <section class="hero">
-    <h2 class="hero__slogan">This is Vissenta</h2>
+    <h2 class="hero__slogan">Vitae Essentia</h2>
     <p class="hero__description">Descubre la esencia entre la naturaleza y tu salud.</p>
 </section>
         
