0fda55c532dcae7f86648f5a663e4eae7001c748
Johan Gavelan <johangavelan@gmail.com>
2020-09-04 14:45:02 -0500

    jquery removed

--- functions.php
+++ functions.php
@@ -6,9 +6,7 @@ function vissentia_scripts() {
     wp_register_style('vissentia_stylesheet', get_template_directory_uri() . '/assets/css/main.css', array(), 1, 'all');
     wp_enqueue_style('vissentia_stylesheet');
 
-    wp_enqueue_script('jquery');
-
-    wp_register_script('vissentia-script', get_template_directory_uri() . '/assets/js/main.js', ['jquery']. 1, true);
+    wp_register_script('vissentia-script', get_template_directory_uri() . '/assets/js/main.js', true);
     wp_enqueue_script('vissentia-script');
 
     wp_enqueue_style('google_web_fonts', 'https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@500&family=Noto+Sans+HK:wght@400;500;700&family=Nunito+Sans:wght@400;600;700;800&display=swap');
