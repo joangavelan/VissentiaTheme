a49f25d2cf7015f858258e13ed52630d0494ed9c
Johan Gavelan <johangavelan@gmail.com>
2020-10-31 16:49:31 -0500

    posts by category section done

--- includes/section-posts-by-category.php
+++ includes/section-posts-by-category.php
@@ -1,104 +1,86 @@
 <section class="posts-bc">
         <div class="posts-bc__col">
+        
             <h3>Superalimentos</h3>
-            
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
-
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
-
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
+                
+            <?php
+            $cat_superalimentos = array(
+                'category' => 10,
+                'numberposts' => 3
+            );
+                
+                $posts = get_posts($cat_superalimentos);    
+                foreach ($posts as $post) :  setup_postdata($post); 
+                ?>
+                    <article>  
+                        <h3 class="entry-title title-sm"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3> 
+                        <div class="entry-date"><?= get_the_date(); ?></div> 
+                    </article>
+                <?php endforeach; 
+            ?>                     
         
         </div>
 
         <div class="posts-bc__col">
             <h3>Plantas</h3>
-            
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
 
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
-
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
+            <?php
+            $cat_plantas_medicinales = array(
+                'category' => 11,
+                'numberposts' => 3
+            );
+                
+                $posts = get_posts($cat_plantas_medicinales);    
+                foreach ($posts as $post) :  setup_postdata($post); 
+                ?>
+                    <article>  
+                        <h3 class="entry-title title-sm"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3> 
+                        <div class="entry-date"><?= get_the_date(); ?></div> 
+                    </article>
+                <?php endforeach; 
+            ?>       
+        
         
         </div>
         <div class="posts-bc__col">
             <h3>Cultiva</h3>
             
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
-
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
-
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
+            <?php
+            $cat_superalimentos = array(
+                'category' => 10,
+                'numberposts' => 3
+            );
+                
+                $posts = get_posts($cat_superalimentos);    
+                foreach ($posts as $post) :  setup_postdata($post); 
+                ?>
+                    <article>  
+                        <h3 class="entry-title title-sm"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3> 
+                        <div class="entry-date"><?= get_the_date(); ?></div> 
+                    </article>
+                <?php endforeach; 
+            ?>
         
         </div>
 
         <div class="posts-bc__col">
             <h3>Tecnología</h3>
             
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
-
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
-
-            <article>
-                <h3 class="entry-title title-sm">
-                    <a href="#">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                </h3>
-                <div class="entry-date">Lun 17, 2020</div>
-            </article>
+            <?php
+            $cat_plantasMedicinales = array(
+                'category' => 11,
+                'numberposts' => 3
+            );
+                
+                $posts = get_posts($cat_plantasMedicinales);    
+                foreach ($posts as $post) :  setup_postdata($post); 
+                ?>
+                    <article>  
+                        <h3 class="entry-title title-sm"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3> 
+                        <div class="entry-date"><?= get_the_date(); ?></div> 
+                    </article>
+                <?php endforeach; 
+            ?> 
         
         </div>
 
