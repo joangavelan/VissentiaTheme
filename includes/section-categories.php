34b447518c61f3c62025748f8334b70f00613ab6
Johan Gavelan <johangavelan@gmail.com>
2020-11-04 23:56:22 -0500

    feed responsive done

--- includes/section-categories.php
+++ includes/section-categories.php
@@ -1,5 +1,5 @@
-<section class="categories m5">
-    <h3 class="sub-heading mb-5">Categorías</h3>
+<section class="categories">
+    <h3 class="sub-heading">Categorías</h3>
     <div class="categories__container">
         <div class="categories__item">
             <h3>Plantas Medicinales</h3>
