fa74b6f86e951f3947036abb5519a95d68d148cc
Johan Gavelan <johangavelan@gmail.com>
2020-11-12 10:51:48 -0500

    categories set

--- includes/section-categories.php
+++ includes/section-categories.php
@@ -1,20 +1,24 @@
 <section class="categories">
     <h3 class="sub-heading">Categorías</h3>
     <div class="categories__container">
-        <div class="categories__item">
+        <a href="https://vissentia.com/plantas-medicinales/" class="categories__item categories__item--1">
+            <img src="http://vissentia.com/wp-content/uploads/2020/11/Plantas-medicinales-category.jpg" alt="plantas medicinales - categoría">
             <h3>Plantas Medicinales</h3>
-        </div>
+        </a>
 
-        <div class="categories__item">
+        <a href="https://vissentia.com/superalimentos/" class="categories__item categories__item--2">
+            <img src="http://vissentia.com/wp-content/uploads/2020/11/superalimentos-category.jpg" alt="superalimentos - categoría">
             <h3>Superalimentos</h3>
-        </div>
+        </a>
 
-        <div class="categories__item">
+        <a href="#" class="categories__item categories__item--3">
+            <img src="http://vissentia.com/wp-content/uploads/2020/11/Cultiva-en-casa-category.jpg" alt="cultiva - categoría">
             <h3>Cultiva en Casa</h3>
-        </div>
+        </a>
 
-        <div class="categories__item">
+        <a href="#" class="categories__item categories__item--4">
+            <img src="http://vissentia.com/wp-content/uploads/2020/11/Tecnologia-category.jpg" alt="tecnología - categoría">
             <h3>Tecnología</h3>
-        </div>
+        </a>
     </div>
 </section>
