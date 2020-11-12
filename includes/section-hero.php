8f59bd03e1a1ea5b9c93f38159ab98eadac79435
Johan Gavelan <johangavelan@gmail.com>
2020-11-12 11:03:57 -0500

    hero content updated

--- includes/section-hero.php
+++ includes/section-hero.php
@@ -2,17 +2,17 @@
     <div class="hero__col-1 hero__cols">
         <article class="entry">
             <div class="entry-image">
-                <a href="demo-blog-single.html"><img id="featured-hero-img" src="https://vissentia.com/wp-content/uploads/2020/07/superalimentos-del-peru-750x425.jpg" alt="13 Superalimentos del Perú"></a>
+                <a href="demo-blog-single.html"><img id="featured-hero-img" src="https://vissentia.com/wp-content/uploads/2020/07/Plantas-medicinales-e1595555584544-750x425.jpg" alt="17 Plantas Medicinales"></a>
             </div>
             <div >
                 <div class="entry-category"><a href="https://vissentia.com/superalimentos/">Superalimentos</a></div>
                 <h3 class="entry-title">
-                    <a href="https://vissentia.com/superalimentos-del-peru/">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
+                    <a href="https://vissentia.com/superalimentos-del-peru/">17 Plantas Medicinales Y Sus Poderosas Propiedades Curativas</a>
                 </h3>
             </div>
             <div class="entry-date">Lunes 13, 2020</div>
             <div class="entry-description">
-                <p>Todo el mundo debería saber de los superalimentos del Perú, poderosos alimentos milenarios altamente beneficiosos para su salud que puede incluir en su dieta diaria. Perú es el tercer país más grande de América del Sur, posee uno de los ecosistemas más prósperos del mundo y micro-climas diversos del mundo...</p>
+                <p>Las plantas medicinales, también conocidas como hierbas medicinales, son conocidas en todo el mundo y utilizadas en todas las culturas como fuente de medicina esencial. Las primeras evidencias de hierbas se descubrieron hace aproximadamente 5000 años, en la civilización sumeria. Desde el principio de los tiempos, la gente utilizaba las plantas para curar las diferentes enfermedades que padecían...</p>
             </div>
         </article>
     </div>
@@ -21,12 +21,12 @@
         <h3 class="sub-heading">Destacados</h3>
             <article class="entry smArt">
                 <div class="entry-image">
-                    <a href="https://vissentia.com/beneficios-de-las-semillas-de-chia/"><img src="https://vissentia.com/wp-content/uploads/2020/09/Semillas-de-Chia-5-Beneficios-de-las-semillas-ricas-en-Omega-3-750x425.jpg" alt="Semillas de Chia"></a>
+                    <a href="https://vissentia.com/beneficios-de-las-semillas-de-chia/"><img src="http://vissentia.com/wp-content/uploads/2020/07/cúrcuma.jpg" alt="Cúrcuma"></a>
                 </div>
                 <div class="entry-content">
-                    <a href="https://vissentia.com/superalimentos/" class="entry-category">Superalimentos</a>
+                    <a href="https://vissentia.com/plantas-medicinales/" class="entry-category">Plantas medicinales</a>
                     <h3 class="entry-title">
-                        <a href="https://vissentia.com/beneficios-de-las-semillas-de-chia/">6 Beneficios de las Semillas de Chía ricas en Omega-3</a>
+                        <a href="https://vissentia.com/beneficios-de-las-semillas-de-chia/">Cúrcuma: 8 Beneficios de este Anti-inflamatorio Natural</a>
                     </h3>
                     <div class="entry-date">Lunes 14, 2020</div>
                 </div>
@@ -47,12 +47,12 @@
 
             <article class="entry smArt">
                 <div class="entry-image">
-                    <a href="https://vissentia.com/plantas-medicinales-y-sus-propiedades-curativas/"><img src="https://vissentia.com/wp-content/uploads/2020/07/Plantas-medicinales-e1595555584544-750x425.jpg" alt="Plantas medicinales"></a>
+                    <a href="https://vissentia.com/plantas-medicinales-y-sus-propiedades-curativas/"><img src="http://vissentia.com/wp-content/uploads/2020/07/superalimentos-del-peru.jpg" alt="13 Superalimentos del Perú"></a>
                 </div>
                 <div class="entry-content">
-                    <a href="https://vissentia.com/plantas-medicinales/" class="entry-category">Plantas Medicinales</a>
+                    <a href="https://vissentia.com/superalimentos/" class="entry-category">Superalimentos</a>
                     <h3 class="entry-title">
-                        <a href="https://vissentia.com/plantas-medicinales-y-sus-propiedades-curativas/">17 Plantas Medicinales Y Sus Poderosas Propiedades Curativas</a>
+                        <a href="https://vissentia.com/plantas-medicinales-y-sus-propiedades-curativas/">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
                     </h3>
                     <div class="entry-date">Lunes 14, 2020</div>
                 </div>
