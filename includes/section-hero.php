fe4ccbea9fba77d01d0146c5030cf23b1b744ea0
Johan Gavelan <johangavelan@gmail.com>
2020-11-15 18:18:04 -0500

    hero col2 posts displayed dynamically

--- includes/section-hero.php
+++ includes/section-hero.php
@@ -1,76 +1,61 @@
 <section class="hero">
+
+    <?php
+        
+        $featured_posts = get_posts(
+            array(
+                'include' => '550, 147, 109, 151',
+                'post_type' => 'post',
+                'orderby' => 'post__in'
+            )
+        )    ;
+
+    ?>
+
     <div class="hero__col-1 hero__cols">
+
         <article class="entry">
             <div class="entry-image">
-                <a href="https://vissentia.com/plantas-medicinales-y-sus-propiedades-curativas/"><img id="featured-hero-img" src="https://vissentia.com/wp-content/uploads/2020/07/Plantas-medicinales-e1595555584544-750x425.jpg" alt="17 Plantas Medicinales"></a>
+                <a href="<?php the_permalink();?>"><img id="featured-hero-img" src="<?php the_post_thumbnail_url();?>" alt="<?php the_title();?>"></a>
             </div>
             <div >
-                <div class="entry-category"><a href="https://vissentia.com/superalimentos/">Superalimentos</a></div>
+                <div class="entry-category"><a href="https://vissentia.com/superalimentos/"><?=the_category();?></a></div>
                 <h3 class="entry-title">
-                    <a href="https://vissentia.com/plantas-medicinales-y-sus-propiedades-curativas/">17 Plantas Medicinales Y Sus Poderosas Propiedades Curativas</a>
+                    <a href="https://vissentia.com/plantas-medicinales-y-sus-propiedades-curativas/"><?php the_title();?></a>
                 </h3>
             </div>
-            <div class="entry-date">Lunes 13, 2020</div>
+            <div class="entry-date"><?=get_the_date();?></div>
             <div class="entry-description">
-                <p>Las plantas medicinales, también conocidas como hierbas medicinales, son conocidas en todo el mundo y utilizadas en todas las culturas como fuente de medicina esencial. Las primeras evidencias de hierbas se descubrieron hace aproximadamente 5000 años, en la civilización sumeria. Desde el principio de los tiempos, la gente utilizaba las plantas para curar las diferentes enfermedades que padecían...</p>
+                <p><?php the_excerpt();?></p>
             </div>
         </article>
+
     </div>
+
 
     <div class="hero__col-2 hero__cols">
         <h3 class="sub-heading">Destacados</h3>
-            <article class="entry smArt">
-                <div class="entry-image">
-                    <a href="https://vissentia.com/beneficios-de-la-curcuma-y-curcumina/"><img src="http://vissentia.com/wp-content/uploads/2020/07/cúrcuma.jpg" alt="Cúrcuma"></a>
-                </div>
-                <div class="entry-content">
-                    <a href="https://vissentia.com/plantas-medicinales/" class="entry-category">Plantas medicinales</a>
-                    <h3 class="entry-title">
-                        <a href="https://vissentia.com/beneficios-de-la-curcuma-y-curcumina/">Cúrcuma: 8 Beneficios de este Anti-inflamatorio Natural</a>
-                    </h3>
-                    <div class="entry-date">Lunes 14, 2020</div>
-                </div>
-            </article>
+
+    <?php if($featured_posts)?>
+
+        <?php foreach ($featured_posts as $post) : setup_postdata($post); ?>
 
             <article class="entry smArt">
                 <div class="entry-image">
-                    <a href="https://vissentia.com/acai-berry-superalimento-propiedades-y-beneficios/"><img src="https://vissentia.com/wp-content/uploads/2020/07/acai-berry-superliamento-750x425.jpg" alt="Acai"></a>
+                    <a href="<?php the_permalink();?>"><img src="<?php the_post_thumbnail_url();?>" alt="<?php the_title();?>"></a>
                 </div>
                 <div class="entry-content">
-                    <a href="https://vissentia.com/superalimentos/" class="entry-category">Superalimentos</a>
+                    <div class="entry-category"><?= the_category(); ?></div>
                     <h3 class="entry-title">
-                        <a href="https://vissentia.com/acai-berry-superalimento-propiedades-y-beneficios/">Açai: 5 Poderosos Beneficios del Fruto Amazónico</a>
+                        <a href="https://vissentia.com/beneficios-de-la-curcuma-y-curcumina/"><?php the_title();?></a>
                     </h3>
-                    <div class="entry-date">Lunes 14, 2020</div>
+                    <div class="entry-date"><?=get_the_date();?></div>
                 </div>
             </article>
 
-            <article class="entry smArt">
-                <div class="entry-image">
-                    <a href="https://vissentia.com/plantas-medicinales-y-sus-propiedades-curativas/"><img src="http://vissentia.com/wp-content/uploads/2020/07/superalimentos-del-peru.jpg" alt="13 Superalimentos del Perú"></a>
-                </div>
-                <div class="entry-content">
-                    <a href="https://vissentia.com/superalimentos/" class="entry-category">Superalimentos</a>
-                    <h3 class="entry-title">
-                        <a href="https://vissentia.com/plantas-medicinales-y-sus-propiedades-curativas/">13 Superalimentos del Perú Consumidos por Tribus Milenarias</a>
-                    </h3>
-                    <div class="entry-date">Lunes 14, 2020</div>
-                </div>
-            </article>
+        <?php endforeach; ?>
 
-            <article class="entry smArt">
-                <div class="entry-image">
-                    <a href="https://vissentia.com/camu-camu-propiedades-y-beneficios/"><img src="https://vissentia.com/wp-content/uploads/2020/07/Camu-Camu-50-60-veces-mas-Vitamina-C-que-la-Naranja-701x425.jpg" alt="Camu Camu"></a>
-                </div>
-                <div class="entry-content">
-                    <a href="https://vissentia.com/superalimentos/" class="entry-category">Superalimentos</a>
-                    <h3 class="entry-title">
-                        <a href="https://vissentia.com/camu-camu-propiedades-y-beneficios/">Camu Camu: 50-60 veces más Vitamina C que la Naranja</a>
-                    </h3>
-                    <div class="entry-date">Lunes 14, 2020</div>
-                </div>
-            </article>
-
-</div>
     </div>
+
+
 </section>
