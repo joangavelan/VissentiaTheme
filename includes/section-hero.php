89a88200d601749d690f1dab4bf11a84997e8ebd
Johan Gavelan <johangavelan@gmail.com>
2020-11-15 19:56:27 -0500

    hero posts set dinamically

--- includes/section-hero.php
+++ includes/section-hero.php
@@ -1,6 +1,8 @@
 <section class="hero">
 
     <?php
+
+        $featured_post = get_post(195);
         
         $featured_posts = get_posts(
             array(
@@ -13,25 +15,22 @@
     ?>
 
     <div class="hero__col-1 hero__cols">
-
         <article class="entry">
             <div class="entry-image">
-                <a href="<?php the_permalink();?>"><img id="featured-hero-img" src="<?php the_post_thumbnail_url();?>" alt="<?php the_title();?>"></a>
+                <a href="<?=get_the_permalink($featured_post);?>"><img id="featured-hero-img" src="<?=get_the_post_thumbnail_url($featured_post);?>" alt="<?php the_title();?>"></a>
             </div>
-            <div >
-                <div class="entry-category"><a href="https://vissentia.com/superalimentos/"><?=the_category();?></a></div>
+            <div>
+                <a class="entry-category" href="<?=get_category_link(11)?>"><?='Plantas Medicinales'?></a>
                 <h3 class="entry-title">
-                    <a href="https://vissentia.com/plantas-medicinales-y-sus-propiedades-curativas/"><?php the_title();?></a>
+                    <a href="<?=get_the_permalink($featured_post)?>"><?=get_the_title($featured_post)?></a>
                 </h3>
             </div>
-            <div class="entry-date"><?=get_the_date();?></div>
+            <div class="entry-date"><?=get_the_date('', $featured_post);?></div>
             <div class="entry-description">
-                <p><?php the_excerpt();?></p>
+                <p>Las plantas medicinales, también conocidas como hierbas medicinales, son conocidas en todo el mundo y utilizadas en todas las culturas como fuente de medicina esencial. Las primeras evidencias de hierbas se descubrieron hace aproximadamente 5000 años, en la civilización sumeria. Desde el principio de los tiempos, la gente utilizaba las plantas para curar las diferentes enfermedades que padecían...</p>
             </div>
         </article>
-
     </div>
-
 
     <div class="hero__col-2 hero__cols">
         <h3 class="sub-heading">Destacados</h3>
@@ -45,9 +44,9 @@
                     <a href="<?php the_permalink();?>"><img src="<?php the_post_thumbnail_url();?>" alt="<?php the_title();?>"></a>
                 </div>
                 <div class="entry-content">
-                    <div class="entry-category"><?= the_category(); ?></div>
+                    <div class="entry-category"><?=the_category();?></div>
                     <h3 class="entry-title">
-                        <a href="https://vissentia.com/beneficios-de-la-curcuma-y-curcumina/"><?php the_title();?></a>
+                        <a href="<?php the_permalink();?>"><?php the_title();?></a>
                     </h3>
                     <div class="entry-date"><?=get_the_date();?></div>
                 </div>
